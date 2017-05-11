90133a78ed8b13d079d634a2d117022d4a229b81
liangxiaojun <liangxiaojun@MacBook-Pro.local>
2017-05-11 13:41:02 +0800

    update

--- examples/config.routes.js
+++ examples/config.routes.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {Route} from 'react-router';
 
-import App from './containers/Root';
+import Root from './containers/Root';
 import FullScreenEditor from './containers/FullScreenEditor';
 import SpecifiedSizeEditor from './containers/SpecifiedSizeEditor';
 
 export default (
-    <Route path="/" component={App}>
+    <Route path="/" component={Root}>
         <Route path="fullscreen" component={FullScreenEditor}/>
         <Route path="specifiedsize" component={SpecifiedSizeEditor}/>
     </Route>
