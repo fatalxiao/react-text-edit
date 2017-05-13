2e5fbc281442cc0c54ee27db7b995a389d0f50ff
liangxiaojun <liangxiaojun@192.168.3.42>
2017-05-13 21:19:45 +0800

    update

--- examples/containers/SpecifiedSizeEditor.js
+++ examples/containers/SpecifiedSizeEditor.js
@@ -9,17 +9,53 @@ import 'assets/sass/SpecifiedSizeEditor.scss';
 export default class SpecifiedSizeEditor extends Component {
 
     constructor(props) {
+
         super(props);
+
+        this.state = {
+            data1: MacDownHelpText,
+            data2: MacDownHelpText
+        };
+
+        this.changeHandle = this::this.changeHandle;
+
+    }
+
+    changeHandle(index, data) {
+        this.setState({
+            [`data${index}`]: data
+        });
     }
 
     render() {
+
+        const {data1, data2} = this.state;
+
         return (
-            <div className="example specified-size-editor-wrapper">
-                <h1>Specified Size</h1>
-                <ReactEditor className="specified-size-editor"
-                             data={MacDownHelpText}
-                             width={500}
-                             heigh={200}/>
+            <div className="specified-size-editor-wrapper">
+
+                <div className="example">
+                    <h1>Specified Size (500 × 200)</h1>
+                    <ReactEditor className="specified-size-editor"
+                                 data={data1}
+                                 width={500}
+                                 heigh={200}
+                                 onChange={(data) => {
+                                     this.changeHandle(1, data);
+                                 }}/>
+                </div>
+
+                <div className="example">
+                    <h1>Specified Size (800 × 400)</h1>
+                    <ReactEditor className="specified-size-editor"
+                                 data={data2}
+                                 width={800}
+                                 heigh={400}
+                                 onChange={(data) => {
+                                     this.changeHandle(2, data);
+                                 }}/>
+                </div>
+
             </div>
         );
     }
