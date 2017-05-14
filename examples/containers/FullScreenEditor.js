8ba004290d620c94fecc036ed2b352779f2df74f
liangxiaojun <liangxiaojun@192.168.3.42>
2017-05-14 14:16:54 +0800

    update

--- examples/containers/FullScreenEditor.js
+++ examples/containers/FullScreenEditor.js
@@ -11,7 +11,7 @@ export default class FullScreenEditor extends Component {
         super(props);
 
         this.state = {
-            data: ''
+            data: MacDownHelpText
         };
 
         this.changeHandle = this::this.changeHandle;
