1112ce25d5428e2f94f421594653837495be7595
Liangxiaojun <fatalxiao@163.com>
2017-08-22 15:33:46 +0800

    update propTypes

--- src/lib/ReactTextEdit.js
+++ src/lib/ReactTextEdit.js
@@ -93,7 +93,12 @@ export default class ReactTextEdit extends Component {
              * the number of scroll bottom blank space height
              * @type {number}
              */
-            scrollBottomBlankHeight: 0
+            scrollBottomBlankHeight: 0,
+
+            /**
+             *
+             */
+            maxColumns: 0
 
         };
 
@@ -202,7 +207,11 @@ ReactTextEdit.propTypes = {
         scrollBarMinLength: PropTypes.number,
         continuousClickInterval: PropTypes.number,
         discontinuousChars: PropTypes.array,
-        showLineNumber: PropTypes.bool
+        showLineNumber: PropTypes.bool,
+        useTabIndent: PropTypes.bool,
+        tabIndentSize: PropTypes.number,
+        scrollBottomBlankHeight: PropTypes.number,
+        maxColumns: PropTypes.number
     }),
 
     /**
