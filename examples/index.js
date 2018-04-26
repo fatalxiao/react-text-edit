8acbd744c73a6ed04575f5792df718968539572c
liangxiaojun <fatalxiao@163.com>
2018-04-26 14:12:59 +0800

    update example

--- examples/index.js
+++ examples/index.js
@@ -3,11 +3,14 @@
 import 'babel-polyfill';
 import React from 'react';
 import {render} from 'react-dom';
+import {HashRouter} from 'react-router-dom';
 import {renderRoutes} from 'react-router-config';
 
 import routes from './config.routes';
 
 render(
-    renderRoutes(routes),
+    <HashRouter>
+        {renderRoutes(routes)}
+    </HashRouter>,
     document.getElementById('app-container')
 );
