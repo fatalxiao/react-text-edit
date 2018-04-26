6065e3fcbb2fb155ba7091a6afccc0ccfef53f3a
liangxiaojun <fatalxiao@163.com>
2018-04-26 11:31:24 +0800

    update build

--- scripts/gulp-mini-package-json.js
+++ scripts/gulp-mini-package-json.js
@@ -1,31 +1,29 @@
 'use strict';
 
-var gutil = require('gulp-util'),
+const gutil = require('gulp-util'),
     through = require('through2');
 
-module.exports = function () {
-    return through.obj(function (chunk, encoding, callback) {
+module.exports = () => through.obj(function (chunk, encoding, callback) {
 
-        if (chunk.isNull()) {
-            callback(null, chunk);
-            return;
-        }
+    if (chunk.isNull()) {
+        callback(null, chunk);
+        return;
+    }
 
-        if (chunk.isStream()) {
-            callback(new gutil.PluginError('gulp-mini-package-json', 'Streaming not supported'));
-            return;
-        }
+    if (chunk.isStream()) {
+        callback(new gutil.PluginError('gulp-mini-package-json', 'Streaming not supported'));
+        return;
+    }
 
-        try {
+    try {
 
-            var data = JSON.parse(chunk.contents.toString());
+        const data = JSON.parse(chunk.contents.toString()),
 
-            var miniData = {
+            miniData = {
                 name: data.name,
                 author: data.author,
                 version: data.version,
                 description: data.description,
-                main: './index.js',
                 keywords: data.keywords,
                 repository: data.repository,
                 license: data.license,
@@ -33,24 +31,23 @@ module.exports = function () {
                 peerDependencies: {
                     'prop-types': data.dependencies['prop-types'],
                     'react': data.dependencies['react'],
-                    'react-dom': data.dependencies['react-dom'],
-                    'react-transition-group': data.dependencies['react-transition-group']
+                    'react-dom': data.dependencies['react-dom']
                 },
                 dependencies: {
-                    'highlight.js': data.dependencies['highlight.js'],
-                    'lodash': data.dependencies['lodash']
+                    'classnames': data.dependencies['classnames'],
+                    'lodash': data.dependencies['lodash'],
+                    'react-transition-group': data.dependencies['react-transition-group']
                 }
             };
 
-            chunk.contents = new Buffer(JSON.stringify(miniData, null, 2));
+        chunk.contents = new Buffer(JSON.stringify(miniData, null, 2));
 
-            this.push(chunk);
+        this.push(chunk);
 
-        } catch (err) {
-            this.emit('error', err);
-        }
+    } catch (e) {
+        this.emit('error', e);
+    }
 
-        callback();
+    callback();
 
-    });
-};
+});
